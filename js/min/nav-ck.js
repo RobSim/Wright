ae05c848fda735f7a8a6a63ecdf3418da54a62d7
Rob Sim <me@robs.im>
2014-04-27 23:43:58 -0700

    Launch

---
+++ js/min/nav-ck.js
@@ -0,0 +1,1 @@
+(function(){var e=document.body,n=e.getElementsByClassName("menu-trigger")[0];"undefined"!=typeof n&&n.addEventListener("click",function(){e.className="menu-active"===e.className?"":"menu-active"})}).call(this);
